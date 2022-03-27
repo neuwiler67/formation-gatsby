3d962c2cccb99bd849d9b0c72e00956cf6379d48
Pierre-Louis <pierre-louis.auvret@digital4better.com>
2022-03-27 12:49:44 +0200

    blog page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,47 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  const pageNames = ["Home", "About", "Blog"];
+  const getPageLinkFromName = (pageName) => {
+    if ("Home" === pageName) {
+      return "/";
+    } else {
+      return "/" + pageName.toLowerCase();
+    }
+  };
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
+          {pageNames.map(pageName => (
+            <li className={navLinkItem}>
+              <Link to={getPageLinkFromName(pageName)} className={navLinkText}>
+                {pageName}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
       <main>
