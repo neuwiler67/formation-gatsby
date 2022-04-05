9b4d7a61dfabb8cfd0156b0271e8dbdf88fb4953
Pierre-Louis <pierre-louis.auvret@digital4better.com>
2022-04-05 20:38:27 +0200

    hide

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,24 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
   },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-plugin-robots-txt",
-      options: {
-        sitemap:
-          (process.env.FRONT_URL || "http://localhost:9000") + "/sitemap.xml",
-        policy: [
-          {
-            userAgent: "*",
-            allow: "/",
-            disallow: [
-              "/servier-centralamericaprod731-admin.servier-university.com.pa/admin/",
-            ],
-          },
-        ],
-      },
-    },
-  ],
+  plugins: [],
 };
