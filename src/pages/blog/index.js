13285bbcfddcffb24d4615bd01c03fa2f6f22164
Pierre-Louis <pierre-louis.auvret@digital4better.com>
2022-04-05 21:26:56 +0200

    add blog posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,28 +1,25 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
-
-export default BlogPage
 
 export const query = graphql`
   query {
@@ -33,8 +30,10 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
 `
+
+export default BlogPage
