4198472bbb9802dd38d486318f25d3b4ead77f9d
Pierre-Louis <pierre-louis.auvret@digital4better.com>
2022-04-06 07:43:12 +0200

    init blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,19 +5,6 @@ import { graphql } from 'gatsby'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-      {
-        data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
-          </article>
-        ))
-      }
-      </ul>
     </Layout>
   )
 }
