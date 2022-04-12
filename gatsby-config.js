9899e9aa6707690ae8f7f6a8142419f757e91f3f
Pierre-Louis <pierre-louis.auvret@digital4better.com>
2022-04-12 15:37:15 +0200

    petit hameau

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,13 @@
+require("dotenv").config({
+  path: `.env`,
+});
+
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
   },
   plugins: [
+    "gatsby-transformer-sharp",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-plugin-mdx",
@@ -13,5 +18,24 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
-  ],
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        collectionTypes: [
+          {
+            name: "editorial-contents",
+            api: { qs: { _locale: "en" } },
+          }
+        ],
+        singleTypes: [
+          {
+            name: "landing",
+            api: { qs: { _locale: "en" } },
+          }
+        ],
+        queryLimit: 1000,
+      },
+    }
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
+import { GatsbyImage } from "gatsby-plugin-image";
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-      <StaticImage
-        alt="A colored drawing of a lovely goose !"
-        src="../images/oie.jpeg"
-      />
+      <h1>{data.strapiLanding.Title}</h1>
+      <GatsbyImage
+            style={{
+              gridArea: "1/1",
+              borderRadius: "0px",
+              maxHeight: "",
+            }}
+            alt="landing"
+            image={
+              data.strapiLanding.headerImage.localFile.childImageSharp.gatsbyImageData
+            }
+            layout="fullWidth"
+          />
+      <p>{data.strapiLanding.Description}</p>
     </Layout>
   )
 }
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+  query {
+    strapiLanding(locale: {eq: "en"}) {
+      headerImage {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+      Title
+      Description
+    }
+  }
+`
